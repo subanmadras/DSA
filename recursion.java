0308075d9676d31581d4f483f2e1edecfef2cb1b
subanmadras <subanmadras@gmail.com>
2023-10-17 16:05:41 +0530

    reverse an array using recursion

--- recursion.java
+++ recursion.java
@@ -1,15 +1,20 @@
 package DSA;
 import java.util.*;
 public class recursion {
-    static void revArr(int n, int arr[]){
-        int narr[] = new int[n];
-        for(int i=n-1; i>=0; i--){
-          narr[n-i-1] = arr[i];  
+    static void revArr(int arr[], int start, int end) {
+        if (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            revArr(arr, start + 1, end - 1);
         }
-        System.out.println(Arrays.toString(narr));
     }
-    public static void main(String args[]){
-        int arr[] = {1,2,3,4,5};
-       revArr(5,arr);
+
+    public static void main(String args[]) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        revArr(arr, 0, n - 1);
+
+        System.out.println(Arrays.toString(arr));
     }
 }
