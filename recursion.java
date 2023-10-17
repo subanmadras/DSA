9ff1aabaffc7090368b1ff986c0f417022544779
subanmadras <subanmadras@gmail.com>
2023-10-17 14:39:31 +0530

    recusrion basic

---
+++ recursion.java
@@ -0,0 +1,13 @@
+package DSA;
+import java.util.*;
+public class recursion {
+    static void func(int n){
+        if(n==0)
+        return;
+        System.out.println(n);
+        func(n-1);
+    }
+    public static void main(String args[]){
+        func(5);
+    }
+}
