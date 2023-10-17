999bd5e0a956e5e346765a0882c3411e2d4a11a7
subanmadras <subanmadras@gmail.com>
2023-10-17 16:27:54 +0530

    plaindrome

--- recursion.java
+++ recursion.java
@@ -1,20 +1,21 @@
 package DSA;
 import java.util.*;
 public class recursion {
-    static void revArr(int arr[], int start, int end) {
-        if (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            revArr(arr, start + 1, end - 1);
-        }
+    static String palindrome(String str, int i,String nstr) {
+       if(i<0){
+        return nstr;
+       }
+        nstr=nstr+str.charAt(i);
+        return palindrome(str,i-1,nstr); 
     }
 
     public static void main(String args[]) {
-        int arr[] = { 1, 2, 3, 4, 5 };
-        int n = arr.length;
-        revArr(arr, 0, n - 1);
-
-        System.out.println(Arrays.toString(arr));
+        String str = "gadag";
+        int n = str.length()-1;
+        String result = palindrome(str,n,"");
+        if(result.equals(str))
+        System.out.println("palindrome");
+        else
+        System.out.println("not a plaindrome");
     }
 }
