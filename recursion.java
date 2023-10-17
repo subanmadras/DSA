7c2017e1f0acf6c4a8e508881d7e3aa4c708968e
subanmadras <subanmadras@gmail.com>
2023-10-17 22:01:08 +0530

    fibonacci series

--- recursion.java
+++ recursion.java
@@ -1,21 +1,16 @@
 package DSA;
+
 import java.util.*;
-public class recursion {
-    static String palindrome(String str, int i,String nstr) {
-       if(i<0){
-        return nstr;
-       }
-        nstr=nstr+str.charAt(i);
-        return palindrome(str,i-1,nstr); 
+ class recursion {
+    static void fib(int a, int b, int n) {
+        if(n==0){
+            return;
+        }
+        System.out.println(a);
+        fib(b, a+b, n-1); 
     }
 
     public static void main(String args[]) {
-        String str = "gadag";
-        int n = str.length()-1;
-        String result = palindrome(str,n,"");
-        if(result.equals(str))
-        System.out.println("palindrome");
-        else
-        System.out.println("not a plaindrome");
+        fib(0,1,8);
     }
 }
