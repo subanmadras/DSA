53c446f024ffb99a5c3e5a4ac954b83a68d5cf9b
subanmadras <subanmadras@gmail.com>
2023-10-17 12:50:33 +0530

    gcd

--- demo.java
+++ demo.java
@@ -1,18 +1,29 @@
 package DSA;
+import java.util.*;
 
 class demo{
     public static void main(String args[]){
-        int n=1221;
-        int num =n; ;
-        int rev = 0;
-        while(n!=0){
-            int rem = n %10;
-            rev = rev * 10 + rem;
-           n = n/10;
+        // int n=1221;
+        // int num =n; ;
+        // int rev = 0;
+        // while(n!=0){
+        //     int rem = n %10;
+        //     rev = rev * 10 + rem;
+        //    n = n/10;
+        // }
+        // if(rev == num)
+        // System.out.println(" number  : "+ rev +" is plaindrome ");
+        // else
+        // System.out.println(" number  : "+ rev +" is not a plaindrome ");
+
+        //gcd of two numbers
+        int n1 =  3;
+        int n2 = 6;
+        int ans = 1;
+        for(int i=1; i<=Math.min(n1,n2); i++){
+            if(n1%i == 0 && n2%i == 0)
+            ans = i;
         }
-        if(rev == num)
-        System.out.println(" number  : "+ rev +" is plaindrome ");
-        else
-        System.out.println(" number  : "+ rev +" is not a plaindrome ");
+        System.out.println("the gcd is "+ans);
     }
 }
