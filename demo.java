dcacc0e0d8019e0e465f23ea04e8e6ba31913686
subanmadras <subanmadras@gmail.com>
2023-10-17 14:04:24 +0530

    print all divisors

--- demo.java
+++ demo.java
@@ -26,16 +26,23 @@ class demo{
         // }
         // System.out.println("the gcd is "+ans);
         //armstrong number
-        int n = 153;
-        int og =n;
-        int ans= 0;
-        while(n!=0){
-            int dig = n % 10;
-            ans = ans+(int)Math.pow(dig,3);
-            n = n/10;
+    //     int n = 153;
+    //     int og =n;
+    //     int ans= 0;
+    //     while(n!=0){
+    //         int dig = n % 10;
+    //         ans = ans+(int)Math.pow(dig,3);
+    //         n = n/10;
+    //     }
+    //     if(og == ans)
+    //    System.out.println("armstrong number");
+    //    else  System.out.println("not an armstrong number");
+    //print all divisors
+    int n=36;
+    for(int i=1; i<=n; i++){
+        if(n%i ==0){
+            System.out.println(i);
         }
-        if(og == ans)
-       System.out.println("armstrong number");
-       else  System.out.println("not an armstrong number"); 
+    } 
     }
 }
