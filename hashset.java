98196a9e8ecbf22b3a9eb97e1a25c9628c3d66d1
subanmadras <subanmadras@gmail.com>
2023-10-18 10:59:03 +0530

    HashSet

---
+++ hashset.java
@@ -0,0 +1,13 @@
+package DSA;
+import java.util.HashSet;
+public class hashset {
+    public static void main(String args[]){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        System.out.println(set);
+    }
+    
+}
