7d073ca1a26c6b88e670a9c308bf1df7de85c6db
subanmadras <subanmadras@gmail.com>
2023-10-19 15:21:31 +0530

    count min and max  freq of elements of array using map

--- minMaxFreq.java
+++ minMaxFreq.java
@@ -2,37 +2,35 @@ package DSA;
 import java.util.*;
 public class minMaxFreq {
     static void minMaxfreq(int arr[], int n){
-        boolean visited[] = new boolean[n];
-        int maxfreq = 0, minfreq = n;
-        int maxEle = 0, minEle = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+       
         for(int i=0; i<n; i++){
-
-            //skip this elemnt if already existed
-            if(visited[i] == true)
-            continue;
-
-            //count frequency
-            int count=1;
-            for(int j=i+1; j<n; j++){
-                if(arr[i] == arr[j]){
-                    visited[j] = true;
-                    count++;   
-                }
+            if(map.containsKey(arr[i]))
+            {
+                map.put(arr[i], map.get(arr[i])+1);
+            }else{
+                map.put(arr[i], 1);
             }
-           // System.out.println(arr[i]+" "+count);
-           if(count>maxfreq){
-            maxEle = arr[i];
-            maxfreq = count;
-           }
-           if(count<minfreq){
-            minEle = arr[i];
-            minfreq = count;
-           }
         }
-        System.out.println("max elemnt "+maxEle+" freq -->"+maxfreq);
+             int maxfreq = 0, minfreq = n;
+             int maxEle = 0, minEle = 0;
+            for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+                int count = entry.getValue();
+                int element = entry.getKey(); 
+                if(count>maxfreq){
+                    maxfreq = count;
+                    maxEle = element;
+                } 
+                if(count<minfreq){
+                    minfreq = count;
+                    minEle = element;
+                } 
+                }
+                System.out.println("max elemnt "+maxEle+" freq -->"+maxfreq);
         System.out.println("min element "+minEle+" freq -->"+minfreq);
+            }
+          
 
-    }
     public static void main(String[] args) {
         int arr[] = {10,20,20,10,30,40,50,40,40,10,70,10,67,10};
         minMaxfreq(arr,arr.length);
